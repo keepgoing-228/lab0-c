5f530190a5aa759cd009a1121085da4d0c16b406
keepgoing-228 <keepdling@gmail.com>
2025-04-14 18:12:02 +0800

    Add: q_new, q_free function
    The q_new function create an empty queue, and
    q_free free all storage of queue.
    
    Change-Id: I0be77449b09bee74302a6ca677455c394b918d1c

--- queue.c
+++ queue.c
@@ -7,11 +7,28 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *new = malloc(sizeof(struct list_head));
+    if (!new)
+        return NULL;
+    INIT_LIST_HEAD(new);
+    return new;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    struct list_head *current, *next;
+    // current will be set to the current node in list_for_each_safe function
+    list_for_each_safe(current, next, head) {
+        element_t *e = list_entry(current, element_t, list);
+        free(e->value);
+        free(e);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
